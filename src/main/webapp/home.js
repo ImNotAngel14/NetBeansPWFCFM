bad900533dc008799b84d220d2b67d66c11379ae
unknown <barbosamane@hotmail.com>
2022-11-24 02:51:52 -0600

    Cargar publicaciones recientes

--- src/main/webapp/home.js
+++ src/main/webapp/home.js
@@ -10,7 +10,7 @@ $.ajax({
         const formato = "data:image/png;base64,";
         document.getElementById('FPHeader').src = formato + data.photo;
         document.getElementById('UserName').innerHTML = data.user.username;
-        document.getElementById('UserPubli').innerHTML = data.user.username;
+        //document.getElementById('UserPubli').innerHTML = data.user.username;
     }
     else
     {
@@ -51,6 +51,11 @@ $(document).ready(function()
     {
         window.location.href = "perfil.html";
     });
+    $(".reactB").click(function()
+    {
+//       alert("Entre");
+//       alert(this.id);
+    });
 });
 
 function loadPosts(data)
@@ -58,35 +63,66 @@ function loadPosts(data)
     alert(data.posts.length);
     for(let i = 0; i < data.posts.length; i++)
     {
-        let str = '<div class="publicacion">';
-        str+='<div class="reaccion">';
-        str+='<div>';
-        str+='<div>';
-        str+='<button class="reactB">';
-        str+='<img src="Img/VotoUp.png">';
-        str+='</button>';
-        str+='</div>';
-        str+='<div>';
-        str+='<span id="contador'+i+'">&nbsp;&nbsp;&nbsp;&nbsp;' + 0 + '</span>';
-        str += '</div>';
-        str+='<div>';
-        str+='<button class="reactB">';
-        str+='<img src="Img/VotoDown.png">';
-        str+='</button>';
+        //////////////
+        //if spoiler create button to hide image
+        ////////////////
+        let str = '<div class="Cpadding"><div class="PubliIndiv">';
+        str+='<div class="Reaccion">';
+        str+='<div><div><button class="reactB ' + data.posts[i].postId + '"';
+        str+='><img src="Img/VotoUp.png" alt=""></button></div>';
+        str+='<div><span>&nbsp;&nbsp;&nbsp;&nbsp;0';
+        str+='</span>';
         str+='</div></div></div>';
-        str+='<div class=post>';
-        str+='<div class=PubliData">';
-        str+='<span class="UserP">' + data.posts[i].username + '</span>';
-        str+='<span class="fechaP">' + data.posts[i].uploadDate + '</span>';
+        str+='<div class="Publi"><div class="PubliData">';
+        str+='<span class="UserP">' + data.posts[i].username + '&nbsp;&nbsp;</span>';
+        str+='<span class="FechaP">' + data.posts[i].uploadDate + '</span>';
         str+='</div>';
         str+='<div class="DescriP">';
         str+='<p>' + data.posts[i].postText + '</p>';
         str+='<br>';
-        str+='<img src="data:image/png;base64,' + data.posts[i].base64PostImage + '">';
-        str+='</div>';
-        str+='<br>';
-        str+='<br>';
-        $("#Publicaciones").append(str);
+        str+='<img class="pngP" src="data:image/png;base64,'+ data.posts[i].base64PostImage + '">';
+        str+='</div><br><br>';
+        str+='<div class="FuncP">';
+        str+='<div class="float-star">';
+        str+='<button class="Fbtn" onclick="showComents()">';
+        str+='<img src="Img/Coment.png" class="icono">';//0
+        str+='</button>&nbsp;';
+        str+='</div></div><br><br><br>';
+        str+='<div class="Comentario" style="display: none;"><br><p>Escribe tu comentario:</p>';
+        str+='<input type="text" class="InputComent" placegolder="¿Qué estás pensando?...">';
+        str+='<br><br>';
+        str+='<button class="BtnComent">Publicar</button>';
+        str+='</div></div></div></div>';
+        
+//        let str = '<div class="publicacion">';
+//        str+='<div class="reaccion">';
+//        str+='<div>';
+//        str+='<div>';
+//        str+='<button class="reactB">';
+//        str+='<img src="Img/VotoUp.png">';
+//        str+='</button>';
+//        str+='</div>';
+//        str+='<div>';
+//        str+='<span id="contador'+i+'">&nbsp;&nbsp;&nbsp;&nbsp;' + 0 + '</span>';
+//        str += '</div>';
+//        str+='<div>';
+//        str+='<button class="reactB">';
+//        str+='<img src="Img/VotoDown.png">';
+//        str+='</button>';
+//        str+='</div></div></div>';
+//        str+='<div class=post>';
+//        str+='<div class=PubliData">';
+//        str+='<span class="UserP">' + data.posts[i].username + '</span>';
+//        str+='<span class="fechaP">' + data.posts[i].uploadDate + '</span>';
+//        str+='</div>';
+//        str+='<div class="DescriP">';
+//        str+='<p>' + data.posts[i].postText + '</p>';
+//        str+='<br>';
+//        str+='<img src="data:image/png;base64,' + data.posts[i].base64PostImage + '">';
+//        str+='</div>';
+//        str+='<br>';
+//        str+='<br>';
+$("#Posts").append(str);
     }
 }
 
